0640dc492345688d6a95a975d61edf18ebd6ba7b
TAISHI SENOH <mokut2017@gmail.com>
2020-06-07 03:19:58 +0900

    UIの"Retry"ボタンでリトライ出来るように。

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIMenu : MonoBehaviour
 {
+    public Canvas canvas;
+    //public Button retryButton;
+
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
+    /* Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.Get(OVRInput.Button.Four))
+        {
+            // 現在のシーン番号を取得
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            // 現在のシーンを再読み込みする
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+    */
+
+    public void OnRetry()
+    {
+        // 現在のシーン番号を取得
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // 現在のシーンを再読み込みする
+        SceneManager.LoadScene(sceneIndex);
+
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [CenterEyeAncher]>[Canvas]
+
+[Button.onClick | Unity](https://docs.unity3d.com/ja/2018.4/ScriptReference/UI.Button-onClick.html)
+ - UIでボタンをクリックするイベントの扱い方。(今回使用していない)
