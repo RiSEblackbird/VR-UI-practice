6dbf84849f44c89d0132b73ec84da0381d4e3c3b
TAISHI SENOH <mokut2017@gmail.com>
2020-06-07 15:41:57 +0900

    物体生成用CS暫定版

---
+++ Assets/Scripts/ButtonSpawnObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ButtonSpawnObject : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject obj;
+    public Canvas canvas;
+
+    public void SpawnObject()
+    {
+        var spawnPosition = new Vector3(player.transform.position.x, player.transform.position.y + 1.5f, player.transform.position.z);
+        var spawnRotation = Quaternion.Euler(UnityEngine.Random.Range(0, 180), UnityEngine.Random.Range(0, 180), UnityEngine.Random.Range(0, 180));
+        GameObject spawnedObj = Instantiate(obj, spawnPosition, spawnRotation) as GameObject;
+        Destroy(spawnedObj, 60.0f);
+
+        Debug.Log(obj.name);
+    }
+}
